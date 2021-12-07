133fb9f263252b5c7ee221784f480d123d899650
GxdTxnz <61706715+GxdTxnz@users.noreply.github.com>
2021-12-07 23:42:41 +0300

    Classic Sorts v2

--- 5/Sorts.java
+++ 5/Sorts.java
@@ -1,60 +1,92 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Sorts 
 {
-	int[] a;
-	public Sorts(int[] a) 
+	public static void BubbleSort(int[] arr) 
 	{
-		this.a = a;
+		for (int i = 0; i < arr.length; i++) 
+		{
+			for (int j = 0; j < arr.length - 1; j++) 
+			{
+				if (arr[j] > arr[j + 1]) 
+				{
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
 	}
 
-	public void bubbleSort() 
+	public static <T extends Comparable<T>> void BubbleSortStr(T[] arr) 
 	{
-		boolean needNextPass = true;
-		for (int i = a.length - 1; i > 0 && needNextPass; i--) 
-		{
-			needNextPass = false;
-			for (int j = 0; j < i; j++) 
-			{
-				if (a[j] > a[j+1]) 
-				{
-					int temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
-					needNextPass = true;
-				}
-			}
-		}
-	}
-	public void insertionSort() 
+        for (int i = 0; i < arr.length; i++) 
+        {
+            for (int j = 0; j < arr.length - 1; j++) 
+            {
+                if (arr[j].compareTo(arr[j + 1]) > 0) 
+                {
+                    T tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+    
+	public static void InsertionSort(int[] arr) 
 	{
-		for (int i = 1; i < a.length; i++) 
-		{
-			int temp = a[i];
-			int j;
-			for (j = i - 1; j >= 0 && temp < a[j]; j --) 
-			{
-				a[j + 1] = a[j];
-			}
-			a[j+1] = temp;
-		}
-	}
-	public void selectionSort() 
-	{
-		for (int i = 0; i < a.length - 1; i ++)
-		{
-			int currenMinIndex = i;
-			for (int j = i + 1; j < a.length; j++)
-			{
-				if (a[currenMinIndex] > a[j])
-				{
-					currenMinIndex = j;
-				}
-			}
-			if (currenMinIndex != i)
-			{
-				int temp = a[i];
-				a[i] = a[currenMinIndex];
-				a[currenMinIndex] = temp;
-			}
-		}
-	}
+        for (int j = 1; j < arr.length; j++) 
+        {
+            int key = arr[j];
+            int i = j - 1;
+            while (i >= 0 && arr[i] > key) 
+            {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = key;
+        }
+    }
+    public static <T extends Comparable<T>> void InsertionSortStr(T[] arr) 
+    {
+        for (int j = 1; j < arr.length; j++) 
+        {
+            T key = arr[j];
+            int i = j - 1;
+            while (i >= 0 && arr[i].compareTo(key) > 0) 
+            {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = key;
+        }
+    }
+    public static void SelectionSort(int[] arr) {
+        for (int min = 0; min < arr.length; min++) {
+            int least = min;
+            for (int j = min + 1; j < arr.length; j++) {
+                if (arr[j] < arr[least])
+                    least = j;
+            }
+            int tmp = arr[min];
+            arr[min] = arr[least];
+            arr[least] = tmp;
+        }
+    }
+    
+    public static <T extends Comparable<T>> void SelectionSortStr(T[] arr) {
+        for (int min = 0; min < arr.length; min++) {
+            int least = min;
+            for (int j = min + 1; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[least]) < 0)
+                    least = j;
+            }
+            T tmp = arr[min];
+            arr[min] = arr[least];
+            arr[least] = tmp;
+        }
+    }
 }
